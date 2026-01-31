af67e4198f95c3aac2fbb7e9d2747b9081492131
YASH THAKUR <tyash2223@gmail.com>
2026-01-31 18:37:05 +0530

    Add files via upload

---
+++ alter_and_droping.sql
@@ -0,0 +1,14 @@
+USE ecom;
+-- ALTER TABLE ecom.clients
+-- ADD COLUMN cast_of_custmore VARCHAR(100);
+
+
+-- ALTER TABLE ecom.clients
+-- RENAME COLUMN city TO city_of_customer;
+
+-- ALTER TABLE ecom.clients
+-- DROP COLUMN cast_of_custmore;
+
+-- delete from ecom.clients;
+
+-- select*from ecom.clients;
